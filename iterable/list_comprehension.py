2cafb6d288d588b85ab763ea43d238ba3a13479e
Desmond <adabedesmond@gmail.com>
2025-04-18 16:32:03 -0700

    modified

--- iterable/list_comprehension.py
+++ iterable/list_comprehension.py
@@ -5,7 +5,19 @@
 # squares = [z*z for z in range(1,11)]
 #
 # print(squares)
+from sympy import print_glsl
 
-fruits = ["apple", "orange", "banana", "coconut"]
-fruits = [fruit.upper() for fruit in fruits]
-print(fruits)
+# fruits = ["apple", "orange", "banana", "coconut"]
+# fruit_chars = [fruit[0] for fruit in fruits]
+# print(fruit_chars)
+
+# numbers = [1, -2, 3, -4, 5, -6]
+# positive_nums = [ num for num in numbers if num >= 0]
+# negative_nums = [ num for num in numbers if num < 0]
+# even_nums = [num for num in numbers if num % 2 ==0]
+# odd_nums = [num for num in numbers if num % 2 ==1]
+# print(odd_nums)
+
+grades =[87,34,56,73,62, 20]
+passing_grades = [ grade for grade in grades if grade >= 60]
+print(grades)
